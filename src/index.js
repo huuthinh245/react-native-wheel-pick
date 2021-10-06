aaba98364f35bd745210b3220f25f06a153f0059
thinhph <thinhph>
2021-10-06 17:20:54 +0700

    remove datepicker

--- src/index.js
+++ src/index.js
@@ -1,20 +1,5 @@
-import { Platform } from 'react-native';
 import Picker from './picker';
-import DatePicker from './date-picker';
-
-let DatePickerComponent = DatePicker;
-
-const registerCustomDatePickerIOS = (CustomDatePickerIOS) => {
-  if (Platform.OS === 'ios') {
-    DatePickerComponent = CustomDatePickerIOS;
-  }
-
-  return DatePickerComponent;
-};
-
 
 export {
-  Picker,
-  DatePickerComponent as DatePicker,
-  registerCustomDatePickerIOS
+  Picker
 }
